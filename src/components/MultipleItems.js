e6ad218e91344f547cb9a9788dbcef62deffaa0a
Aidarus Badawy <aidarusbadawy@gmail.com>
2021-08-17 10:58:08 +0300

    navbar modified

--- src/components/MultipleItems.js
+++ src/components/MultipleItems.js
@@ -1,6 +1,12 @@
 import React, { Component } from "react";
 import Slider from "react-slick";
 import {RiArrowRightCircleLine, RiArrowLeftCircleLine } from 'react-icons/ri'
+import imagehome1 from "../images/imagehome1.jpg"
+import imagehome2 from "../images/imagehome2.jpg"
+import imagehome3 from "../images/imagehome3.jpg"
+import imagehome4 from "../images/imagehome4.jpg"
+import imagehome5 from "../images/imagehome5.jpg"
+
 
 
 export default class MultipleItems extends Component {
@@ -32,35 +38,35 @@ export default class MultipleItems extends Component {
         </div>
         <Slider className='slider' ref={c => (this.slider = c)} {...settings}>
           <div key={1}>
-            <img className='image' alt='carousel-img-one' src='https://images.unsplash.com/photo-1580394640019-00d34094ae13?ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8a2VueWF8ZW58MHwxfDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60'/>
+            <img className='image' alt='carousel-img-one' src={imagehome1}/>
             <div className='slider-detail'>
             <h3 className='text-one'>01</h3>
             <h3 className='text-two'>CULTURES</h3>
             </div>
           </div>
           <div key={2}>
-            <img className='image' alt='carousel-img-two' src='https://images.unsplash.com/photo-1511216113906-8f57bb83e776?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fGtlbnlhfGVufDB8MXwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60'/>
+            <img className='image' alt='carousel-img-two' src={imagehome2}/>
             <div className='slider-detail'>
             <h3 className='text-one'>02</h3>
             <h3 className='text-two'>WILDLIFE</h3>
             </div>
           </div>
           <div key={3}>
-            <img className='image' alt='carousel-img-three' src='https://images.unsplash.com/photo-1558462610-cf8d1b0248a0?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fGtlbnlhfGVufDB8MXwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60'/>
+            <img className='image' alt='carousel-img-three' src={imagehome3}/>
             <div className='slider-detail'>
             <h3 className='text-one'>03</h3>
             <h3 className='text-two'>SCENERY</h3>
             </div>
           </div>
           <div key={4}>
-            <img className='image' alt='carousel-img-four' src='https://images.unsplash.com/photo-1591894509537-c42013bc8bbd?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjF8fGtlbnlhfGVufDB8MXwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60'/>
+            <img className='image' alt='carousel-img-four' src={imagehome4}/>
             <div className='slider-detail'>
             <h3 className='text-one'>04</h3>
             <h3 className='text-two'>ADVENTURES</h3>
             </div>
           </div>
           <div key={5}>
-            <img className='image' alt='carousel-img-five' src='https://images.unsplash.com/photo-1506953823976-52e1fdc0149a?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YmVhY2h8ZW58MHwxfDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60'/>
+            <img className='image' alt='carousel-img-five' src={imagehome5}/>
             <div className='slider-detail'>
             <h3 className='text-one'>05</h3>
             <h3 className='text-two'>BEACHES</h3>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,21 +1,48 @@
 import {Link} from 'react-scroll'
+import {useState} from 'react'
+
 
 function Navbar() {
+    const [nav , setNav] = useState(false);
+
+    const changeBackground = () => {
+        if(window.scrollY >= 50){
+            setNav(true);
+        }
+        else {
+            setNav(false);
+        }
+    }
+    window.addEventListener('scroll', changeBackground)
     return (
-        <div className='hero'>
-            <div className='content'>
-                <h2 className='logo'>Travel<span>Kenya</span></h2>
-                <ul className='nav-ul'>
-                    <Link to='statistics'smooth={true} duration={1000}><li>About</li></Link>
-                    <Link to='attractions'><li>Attractions</li></Link>
-                    <Link href='tours'><li>Tours</li></Link>
-                    <Link href='question'><li>Q&A</li></Link>
-                    <Link href='contact'><li>Contact Us</li></Link>
-                </ul>
-            </div>
-            
-            
-        </div>
+        <nav className={nav ? 'nav active' : 'nav'}>
+            <h2 className='logo'>Travel<span>Kenya</span></h2>
+            <input type='checkbox' className='menu-btn' id='menu-btn'/>
+            <label className='menu-icon' for='menu-btn'>
+                <span className='nav-icon'></span>
+            </label>
+            <ul className='menu'>
+                <li><a href='#'>Home</a></li>
+                <li><a href='#'>About</a></li>
+                <li><a href='#'>Attractions</a></li>
+                <li><a href='#'>Tours</a></li>
+                <li><a href='#'>Contact Us</a></li>
+            </ul>
+        </nav>
+        // <div className='hero'>
+        //     <div className='content'>
+        //         <h2 className='logo'>Travel<span>Kenya</span></h2>
+        //         <ul className='nav-ul'>
+        //             <Link to='statistics'smooth={true} duration={1000}><li>About</li></Link>
+        //             <Link to='attractions'><li>Attractions</li></Link>
+        //             <Link href='tours'><li>Tours</li></Link>
+        //             <Link href='question'><li>Q&A</li></Link>
+        //             <Link href='contact'><li>Contact Us</li></Link>
+        //         </ul>
+        //     </div>
+        // </div>
+
+        
     )
 }
 
